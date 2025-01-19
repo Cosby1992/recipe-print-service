78be8b2ae2570f5b6d97a95bb8805d7244d9f4c9
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-19 14:52:37 +0100

    feat(api): change route name

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -3,6 +3,6 @@ const router = express.Router()
 
 // Attach middleware specific to this router here if necessary
 
-export const recipeRouter = router.post('/print-friendly', (req, res) => {
+export const recipeRouter = router.post('/extract-from-url', (req, res) => {
     res.send("[POST '/'] working!");
 });
