60c4dbfdd158183d60815a8922cfc7cee837e263
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-19 16:05:18 +0100

    feat(api): implemented http status constants

---
+++ src/constants/http-status.ts
@@ -0,0 +1,19 @@
+export enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export const HttpStatusMessages: Record<HttpStatus, string> = {
+  [HttpStatus.OK]: "OK",
+  [HttpStatus.CREATED]: "Created",
+  [HttpStatus.BAD_REQUEST]: "Bad Request",
+  [HttpStatus.UNAUTHORIZED]: "Unauthorized",
+  [HttpStatus.FORBIDDEN]: "Forbidden",
+  [HttpStatus.NOT_FOUND]: "Not Found",
+  [HttpStatus.INTERNAL_SERVER_ERROR]: "Internal Server Error",
+} as const;
