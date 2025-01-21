e353b1f7ab7916a2928c6ca6ab6f05a562084f1a
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-21 18:29:41 +0100

    feat(api): added body parser middleware to handle json request body

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import { recipeRouter } from "./routes/recipe";
 const app = express();
 const port = 3000;
 
+// Body parser
+app.use(express.json());
+
+// Attach routes
 app.use('/recipe', recipeRouter);
 
 app.listen(port, () => {
