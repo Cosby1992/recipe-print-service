c546a3b32b8685ee7eb6b3ecd0928a0c5bca1b0a
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-21 18:35:07 +0100

    feat(api): now gets html from provided url

--- src/routes/recipe.ts
+++ src/routes/recipe.ts
@@ -14,6 +14,16 @@ export const recipeRouter = router.post(
     const { url, targetPortions } = req.body as RecipeRequestDto;
 
     // call and "verify" provided url and store html in mem
+    const html = await getHTML(url);
+
+    if (!html) {
+      res.status(HttpStatus.BAD_REQUEST).json({
+        message: HttpStatusMessages[HttpStatus.BAD_REQUEST],
+        status: HttpStatus.BAD_REQUEST,
+        errors: [`Failed to get html from: "${url}"`],
+      } satisfies ErrorResponse);
+      return;
+    }
 
     // Send predefined prompt to chat gpt for converting the recepi to json
 
@@ -21,5 +31,21 @@ export const recipeRouter = router.post(
 
     // return json dto with portions, ingredients, preparing steps, etc..
 
+    res.json({ "hot": "damn"});
   }
 );
+
+const getHTML = async (url: string): Promise<string | void> => {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return;
+    }
+
+    const body = await response.text();
+
+    return body.includes("<html") ? body : undefined;
+  } catch (error) {
+    console.log(error);
+  }
+};
