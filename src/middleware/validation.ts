84693d3209593fa0efbe03379d7ac869b95b0b65
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-26 18:41:30 +0100

    refactor(validation): now throws error to custom global error handler

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,16 +1,20 @@
 import { plainToInstance } from "class-transformer";
 import { validate } from "class-validator";
 import { NextFunction, Request, RequestHandler, Response } from "express";
-import { HttpStatus, HttpStatusMessages } from "../constants/http-status";
-import { ErrorResponse } from "../dtos/error-response.dto";
+import {
+  BadRequestError,
+  HttpError,
+  InternalServerError,
+} from "../exceptions/http-error";
+import { expressAsyncHandler } from "../utils/express-async-handler";
 
 /**
  * Validates a class using class-validator and class-transformer
  * next() is called if validation passes, else an error response is send to client
- * @param type A class to validate containing decorators or transforms 
+ * @param type A class to validate containing decorators or transforms
  * by class-validator or class-tranformer
  * @returns (RequestHandler) a middleware function
- * 
+ *
  * @example
  * router.post(
  *      "/extract-from-url",
@@ -20,13 +24,11 @@ import { ErrorResponse } from "../dtos/error-response.dto";
  *      }
  * );
  */
-export const validationMiddleware = <T extends object>(type: new () => T): RequestHandler => {
-  if (!type || typeof type !== "function") {
-    throw new Error("Invalid DTO class provided to validationMiddleware");
-  }
-
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
+export const validationMiddleware = <T extends object>(
+  type: new () => T
+): RequestHandler => {
+return expressAsyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
       const dtoInstance = plainToInstance(type, req.body);
 
       const errors = await validate(dtoInstance, {
@@ -41,35 +43,10 @@ export const validationMiddleware = <T extends object>(type: new () => T): Reque
           Object.values(error.constraints || {})
         );
 
-        res.status(HttpStatus.BAD_REQUEST).json({
-          message: HttpStatusMessages[HttpStatus.BAD_REQUEST],
-          status: HttpStatus.BAD_REQUEST,
-          errors: validationErrors,
-        } satisfies ErrorResponse);
-
-        return;
+        throw new BadRequestError(validationErrors);
       }
 
       next();
-    } catch (error) {
-      console.error(error);
-      if (error instanceof SyntaxError) {
-        // Handle json parse error
-        res.status(HttpStatus.BAD_REQUEST).json({
-          message: HttpStatusMessages[HttpStatus.BAD_REQUEST],
-          status: HttpStatus.BAD_REQUEST,
-          errors: ["Failed to parse request body"],
-        } satisfies ErrorResponse);
-        return;
-      }
-
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: HttpStatusMessages[HttpStatus.INTERNAL_SERVER_ERROR],
-        status: HttpStatus.INTERNAL_SERVER_ERROR,
-        errors: [],
-      } satisfies ErrorResponse);
-
-      return;
     }
-  };
+  );
 };
