0ca643d54121db0e84a257f1042a1163da791d58
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-26 18:29:38 +0100

    feat(errors): added a few custom errors for better api error responses

---
+++ src/exceptions/http-error.ts
@@ -0,0 +1,43 @@
+import { HttpStatus, HttpStatusMessages } from "../constants/http-status";
+import { ErrorResponseDto } from "../dtos/error-response.dto";
+
+export class HttpError extends Error implements ErrorResponseDto {
+  public readonly status: HttpStatus;
+  public readonly errors: string[];
+
+  constructor(status: HttpStatus, statusText: string, errors: string[] = ['Unknown error']) {
+    super(statusText);
+    this.status = status;
+    this.errors = errors;
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor(errors?: string[] | undefined) {
+    super(
+      HttpStatus.InternalServerError,
+      HttpStatusMessages[HttpStatus.InternalServerError],
+      errors
+    );
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(errors?: string[]) {
+    super(
+      HttpStatus.BadRequest,
+      HttpStatusMessages[HttpStatus.BadRequest],
+      errors
+    );
+  }
+}
+
+export class NotImplementedError extends HttpError {
+    constructor(errors?: string[]) {
+        super(
+          HttpStatus.NotImplemented,
+          HttpStatusMessages[HttpStatus.NotImplemented],
+          errors
+        );
+      }
+}
