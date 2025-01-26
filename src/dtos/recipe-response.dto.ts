04fd279169bf49aa3de36cd37ed1dca59a8a7777
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-26 18:40:01 +0100

    feat(dto): added recipe-response dto to handle array of recipies which is final result

---
+++ src/dtos/recipe-response.dto.ts
@@ -0,0 +1,10 @@
+import { IsArray, ValidateNested } from "class-validator";
+import { RecipeDTO } from "./recipe.dto";
+import { Type } from "class-transformer";
+
+export class RecipesResponseDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => RecipeDTO)
+  recipes: RecipeDTO[];
+}
