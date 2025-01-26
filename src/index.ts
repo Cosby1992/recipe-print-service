aacc24c6942a4b01b929bde605c3d908dde5bfde
cosby1992 <andersbirkedahl@hotmail.com>
2025-01-26 18:36:55 +0100

    feat(env): added dotenv import and refactored to be in function which is called on startup

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,30 @@
+import 'reflect-metadata';
 import express from "express";
 import { recipeRouter } from "./routes/recipe";
+import dotenv from "dotenv";
 import { requestLogMiddleware } from './middleware/request-log';
 import { globalErrorHandlerMiddleware } from './middleware/global-error-handler';
 
-// Load environment variables
-const result = dotenv.config();
-if (result.error) {
-  console.error('Error loading .env:', result.error);
-}
-
-const app = express();
-const port = 3000;
-
+function start() {
+  // Load environment variables
+  const result = dotenv.config();
+  if (result.error) {
+    console.error("Error loading .env:", result.error);
+  }
+  
+  const app = express();
+  
   app.use(express.json()); // Body parser
   app.use(requestLogMiddleware);
-
-// Attach routes
+  
+  // Attach routes
+  app.use("/recipe", recipeRouter);
   app.use(globalErrorHandlerMiddleware);
 
-app.listen(port, () => {
+  const port = 3000;
+  app.listen(port, () => {
     console.log(`Server is litening on port: ${port}`);
-})
+  });
+}
+
+start();
